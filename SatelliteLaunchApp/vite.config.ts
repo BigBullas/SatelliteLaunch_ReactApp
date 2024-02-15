72f4bb713ccca4d87fcd267beb7cf9590193e208
Alik Nigmatullin <NigAlik020503@yandex.ru>
2024-02-15 19:06:48 +0300

    added base

--- SatelliteLaunchApp/vite.config.ts
+++ SatelliteLaunchApp/vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  // base: 'SatelliteLaunch_ReactApp/'
+  base: 'SatelliteLaunch_ReactApp/',
   plugins: [
     react(), 
     VitePWA({ 
