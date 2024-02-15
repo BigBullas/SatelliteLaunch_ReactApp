336cbd11734f37b297cc977842fba80ddae136fc
Alik Nigmatullin <NigAlik020503@yandex.ru>
2024-02-15 19:07:59 +0300

    changed router to Hash

--- SatelliteLaunchApp/src/App.tsx
+++ SatelliteLaunchApp/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom'
 import SinglePayloadPage from './pages/SinglePayloadPage'
 import PayloadsPage from './pages/PayloadsPage'
 import Header from './components/Header/Header'
@@ -13,17 +13,17 @@ const App: React.FC = () => {
     setPath(newPath);
   }
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Breadcrump desc={ desc } path= { path } />
       <Routes>
         <Route path="/" element={<PayloadsPage changeBreadcrump = {changeBreadcrump} />} />
         <Route path="/payloads/:id" element={<SinglePayloadPage changeBreadcrump = {changeBreadcrump} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
-{/* </HashRouter> */}
+{/* </ BrowserRouter> */}
 
 export default App
